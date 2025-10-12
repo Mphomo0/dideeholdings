1063667fbf8cd65708e8ec234bff676cef58853e
Mphomo0 <mphomoipolai1@gmail.com>
2025-10-12 10:27:37 +0200

    added GA code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/global/Navbar'
 import Footer from '@/components/global/Footer'
+import GoogleAnalytics from '@/components/global/GoogleAnalytics'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -15,9 +16,9 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'Didee Holdings - Building Your Future',
+  title: 'Didee Holdings - We Build Your Future',
   description:
-    'Professional construction services wit a focus to safety, quality, and realiability. Serving South Africa since 2018',
+    'Professional construction services wit a focus to safety, quality, and realiability. Serving Centurion and Northern Cape since 2018',
 }
 
 export default function RootLayout({
@@ -26,10 +27,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
         <Navbar />
         {children}
         <Footer />
